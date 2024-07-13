8f77399a5a6ab681c2b7c0ae782db475e2cf88de
IscJlchavezG <joseluis@iscjlchavezg.mx>
2024-07-13 13:09:16 -0600

    correccion de practica

--- SuperMercado/add_productos.php
+++ SuperMercado/add_productos.php
@@ -4,9 +4,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     $Nombre = $_POST['nombre'];
     $Precio = $_POST['precio'];
 
-    echo "El nombre del producto es: ".$Nombre." El precio es: ".$Precio;
-
-
 }
 
 
