7489131c1c7dd63502af7b4f628601d38f407855
IscJlchavezG <joseluis@iscjlchavezg.mx>
2024-07-20 11:10:37 -0600

    update se inserto ya los productos

--- SuperMercado/add_productos.php
+++ SuperMercado/add_productos.php
@@ -3,7 +3,25 @@ include 'app/conecta.php';
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     $Nombre = $_POST['nombre'];
     $Precio = $_POST['precio'];
-
+    // prueba para ver si recuperamos datos 
+    // echo "El Nombre del producto es: ".$Nombre." y el precio es: ".$Precio;
+    if($Nombre == ""){
+        echo "Por favor coloca el nombre del producto";
+    }
+    else if($Precio == ""){
+       echo "Por favor Coloca el Precio del producto";
+    }
+    else{
+     // insertar los datos dentro de la base de datos
+     $Rproducto = "INSERT INTO Productos(NombreProducto,Precio)VALUES('$Nombre','$Precio')";
+     $ERproducto = $conecta->query($Rproducto );
+     if($ERproducto > 0){
+         echo "Registro exitoso";
+     }
+     else{
+        echo "No se registro en la base de datos";
+     }
+    }
 }
 
 
